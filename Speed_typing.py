24cd1b60abee695997f5af1cd4e4dc66b1a2afda
Prisha <prishas2007@gmail.com>
2025-07-21 13:46:57 -0700

    uploading typing test

---
+++ Speed_typing.py
@@ -0,0 +1,81 @@
+import curses
+from curses import wrapper
+import time
+import random
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Welcome to the Speed Typing Test!")
+    stdscr.addstr(1, 0, "Press any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(0, 0, target, curses.color_pair(3))
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(2, i, char, color)
+
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
+def wpm_test(stdscr):
+    target_text = load_text()
+    current_text = []
+    start_time = time.time()
+    stdscr.nodelay(True)
+    wpm = 0
+    finished = False
+
+    while True:
+        if not finished:
+            time_elapsed = max(time.time() - start_time, 1)
+            wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
+
+        if "".join(current_text) == target_text and not finished:
+            stdscr.nodelay(False)
+            finished = True
+
+        if finished:
+            continue
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+
+        if ord(key) == 27:  # ESC to quit
+            break
+
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Correct character
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)    # Incorrect character
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Target text
+
+    start_screen(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(4, 0, "You completed the text! Press any key to play again, or ESC to exit.")
+        key = stdscr.getkey()
+        if ord(key) == 27:  # ESC key
+            break
+
+wrapper(main)
